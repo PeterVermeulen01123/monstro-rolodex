f3d80833c00a796cc2e8f1aac1204de46fa971cc
PeterVermeulen01123 <94803849+PeterVermeulen01123@users.noreply.github.com>
2022-01-22 14:05:43 -0500

    updated a div wrapper

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import {Card} from '../card/card.component.jsx';
+
+import './card-list.styles.css';
+
+export const CardList = (props) => {
+    
+        return <div className='card-list'>
+
+            {props.monsters.map((monster, i) => (
+                <Card key={i} monster={monster} />
+            ))}
+
+        </div>
+
+}
+
